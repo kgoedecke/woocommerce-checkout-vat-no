a78c8e825ffa2d1ee144af9be1fc6b9402d46ccb
cheh <chehovskiy.dima@gmail.com>
2018-10-11 18:45:55 +0300

    ADD: error message

--- class-woocommerce-checkout-vat-no-plugin.php
+++ class-woocommerce-checkout-vat-no-plugin.php
@@ -4,7 +4,7 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 
 		/**
 		 * A reference to an instance of this class.
-		 * 
+		 *
 		 * @since 1.0.0
 		 * @var   object
 		 */
@@ -12,7 +12,7 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 
 		/**
 		 * Returns an instance of this class.
-		 * 
+		 *
 		 * @since  1.0.0
 		 * @return object A single instance of this class.
 		 */
@@ -27,7 +27,7 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 
 		/**
 		 * Initializes the plugin by setting filters and administration functions.
-		 * 
+		 *
 		 * @since 1.0.0
 		 */
 		private function __construct() {
@@ -38,7 +38,8 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 			add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'update_order_meta' ), 11, 2 );
 
 			add_action( 'woocommerce_admin_order_data_after_billing_address', array( $this, 'add_vat_number_to_admin' ) );
-			add_action( 'woocommerce_process_shop_order_meta', array( $this, 'save_order_meta' ) );
+			add_action( 'save_post', array( $this, 'save_order_meta' ), 10, 3 );
+			add_action( 'admin_notices', array( $this, 'admin_notices' ) );
 
 			add_action( 'woocommerce_after_checkout_validation', array( $this, 'checkout_validation' ), 10, 2 );
 		}
@@ -62,14 +63,14 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 		 * @since 1.0.0
 		 */
 		public function assets() {
-		
+
 			// This check says that it's not checkout page.
 			if ( ! is_checkout() ) {
 				return;
 			}
 
 			$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-		
+
 			wp_enqueue_script( 'woo-checkout-vat-no-script', plugin_dir_url( __FILE__ ) . "js/scripts{$min}.js", array( 'jquery' ), '1.0.0', true );
 			wp_enqueue_style( 'woo-checkout-vat-no-style', plugin_dir_url( __FILE__ ) . 'css/styles.css', array(), '1.0.0' );
 		}
@@ -89,7 +90,7 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 				'clear'       => true,
 				'priority'    => 35,
 			);
-		
+
 			return $fields;
 		}
 
@@ -114,7 +115,7 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 		 */
 		public function add_vat_number_to_admin( $order ) {
 			$vat_number = get_post_meta( $order->get_id(), 'vat_number', true );
-		
+
 			if ( ! empty( $vat_number ) ) : ?>
 				<div class="address">
 					<p>
@@ -123,7 +124,7 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 					</p>
 				</div>
 			<?php endif; ?>
-		
+
 			<div class="edit_address"><?php
 				woocommerce_wp_text_input( array(
 					'id'            => 'vat_number',
@@ -142,14 +143,49 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 		 * @since 1.0.0
 		 * @param int $order_id
 		 */
-		public function save_order_meta( $order_id ) {
+		public function save_order_meta( $post_id, $post, $update ) {
+			$do_autosave = defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE;
+			$is_autosave = wp_is_post_autosave( $post_id );
+			$is_revision = wp_is_post_revision( $post_id );
+
+			if ( $do_autosave || $is_autosave || $is_revision ) {
+				return;
+			}
+
+			$post_type = get_post_type( $post );
+
+			if ( 'shop_order' !== $post_type ) {
+				return;
+			}
 
 			if ( isset( $_POST[ 'vat_number' ] ) ) {
-				$result = $this->vat_no_validation( wc_clean( $_POST['vat_number'] ) );
-		
-				if ( $result ) {
-					update_post_meta( $order_id, 'vat_number', wc_clean( $_POST[ 'vat_number' ] ) );
+				$vat_number = wc_clean( $_POST['vat_number'] );
+
+				if ( '' === $vat_number ) {
+					delete_post_meta( $post_id, 'vat_number' );
+					return;
 				}
+
+				$result = $this->vat_no_validation( $vat_number );
+
+				if ( $result ) {
+					update_post_meta( $post_id, 'vat_number', $vat_number );
+				} else {
+					add_option( 'woo_checkout_vat_no_show_fail_notice', true, '', 'no' );
+				}
+			}
+		}
+
+		/**
+		 * Show admin notice if VAT number is not valid.
+		 *
+		 * @since 1.0.0
+		 */
+		public function admin_notices() {
+			if ( get_option( 'woo_checkout_vat_no_show_fail_notice' ) ) {
+				$message = sprintf( '<strong>%1$s</strong> %2$s', esc_html__( 'Error:', 'woocommerce-checkout-vat-no' ), esc_html__( 'is not a valid VAT number.', 'woocommerce-checkout-vat-no' ) );
+				echo '<div class="notice notice-error is-dismissible"><p>'. $message .'</p></div>';
+				delete_option( 'woo_checkout_vat_no_show_fail_notice' );
 			}
 		}
 
@@ -164,7 +200,7 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 
 			if ( ! empty( $data['vat_number'] ) ) {
 				$result = $this->vat_no_validation( $data['vat_number'] );
-		
+
 				if ( ! $result ) {
 					$errors->add( 'validation', sprintf( '<strong>%1$s</strong> %2$s', esc_html__( 'VAT No.', 'woocommerce-checkout-vat-no' ), esc_html__( 'is not a valid.', 'woocommerce-checkout-vat-no' ) ) );
 				}
@@ -180,9 +216,9 @@ if ( ! class_exists( 'WooCommerce_Checkout_Vat_No_Plugin' ) ) {
 		 */
 		public function vat_no_validation( $check_it ) {
 			require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
-		
+
 			$validator = new DvK\Vat\Validator();
-		
+
 			return $validator->validateFormat( $check_it );
 		}
 	}
